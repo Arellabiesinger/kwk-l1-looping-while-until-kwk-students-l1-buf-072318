17f835eda85e3569d2929b5b2ecd14e2167afebc
Arellabiesinger <arellabiesinger@gmail.com>
2018-07-25 13:37:38 +0000

    automatically backed up by learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,6 +1,10 @@
 def using_until
-        levitation_force = 6
-        
+        levitation_force < 10 
+   puts "Wingardium Leviosa"
+ 
+ 
+ end  
+   #First, we'll write a while loop that will continue to `puts` the phrase "Wingardium Leviosa" while our levitation force is less than `10`. Every time we `puts` that phrase, we should increment our levitation force by `1`.     
     
-end
+
 
